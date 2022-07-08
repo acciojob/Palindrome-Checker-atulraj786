020065c2c66c2e00bd1037cce82a0c683e8acbc0
atulraj786 <107700182+atulraj786@users.noreply.github.com>
2022-07-09 00:45:32 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+  
+ str = str.toLowerCase().replace(/[^a-z0-9]/gi,'');
+  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
+    if (str.charAt(i) !== str.charAt(j)) return false;
+  }
+  return true;
 
 }
 module.exports = palindrome
